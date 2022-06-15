13383cfe3ef4976f6d01b1c513a6525ab0f04e14
Michal Dudkiewicz <michal.dudkiewicz96@gmail.com>
2022-06-15 13:47:49 +0200

    stars feature

--- Assets/Scripts/SelectLevel.cs
+++ Assets/Scripts/SelectLevel.cs
@@ -8,6 +8,8 @@ public class SelectLevel : MonoBehaviour
 {
     public Dictionary<int, bool> levels;
     public List<Sprite> unlockedSprite;
+    public GameObject unlockedStarPrefab;
+    public GameObject lockedStarPrefab;
 
     public List<GameObject> levelsButtons;
 
@@ -15,8 +17,7 @@ public class SelectLevel : MonoBehaviour
     void OnEnable()
     {
         levels = new Dictionary<int, bool>();
-        levels[1] = true;
-        levelsButtons[0].GetComponent<Image>().sprite = unlockedSprite[0];
+
         levelsButtons[0].GetComponent<Button>().onClick.AddListener(() => ButtonClicked(1));
         levelsButtons[1].GetComponent<Button>().onClick.AddListener(() => ButtonClicked(2));
         levelsButtons[2].GetComponent<Button>().onClick.AddListener(() => ButtonClicked(3));
@@ -26,15 +27,54 @@ public class SelectLevel : MonoBehaviour
         levelsButtons[6].GetComponent<Button>().onClick.AddListener(() => ButtonClicked(7));
         levelsButtons[7].GetComponent<Button>().onClick.AddListener(() => ButtonClicked(8));
         levelsButtons[8].GetComponent<Button>().onClick.AddListener(() => ButtonClicked(9));
-        for (int i = 2; i <=9; i++)
+        for (int i = 1; i <=9; i++)
         {
             int levelUnlocked = PlayerPrefs.GetInt("level" + i.ToString());
-            bool isLevelUnlocked = levelUnlocked == 1;
+            bool isLevelUnlocked = levelUnlocked == 1 || i == 1;
             levels.Add(i, isLevelUnlocked);
 
             if (isLevelUnlocked)
             {
                 levelsButtons[i - 1].GetComponent<Image>().sprite = unlockedSprite[i - 1];
+                string key = i.ToString() + "stars";
+                int stars = PlayerPrefs.GetInt(key);
+
+                float width = levelsButtons[i - 1].GetComponent<Button>().targetGraphic.GetComponent<Image>().rectTransform.rect.width * Screen.dpi/640;
+                float height = levelsButtons[i - 1].GetComponent<Button>().targetGraphic.GetComponent<Image>().rectTransform.rect.height * Screen.dpi / 640;
+
+                Vector3 first = new Vector3(width/4, 0.4f * height, 0);
+                Vector3 firstPosition = levelsButtons[i - 1].transform.position - first;
+
+                Vector3 second = new Vector3(0.0f, 0.35f * height, 0);
+                Vector3 secondPosition = levelsButtons[i - 1].transform.position - second;
+
+                Vector3 third = new Vector3(-width/4, 0.4f * height, 0);
+                Vector3 thirdPosition = levelsButtons[i - 1].transform.position - third;
+
+                switch (stars)
+                {
+                    case 1:
+                        Instantiate(unlockedStarPrefab, firstPosition, Quaternion.identity, levelsButtons[i - 1].transform);
+                        Instantiate(lockedStarPrefab, secondPosition, Quaternion.identity, levelsButtons[i - 1].transform);
+                        Instantiate(lockedStarPrefab, thirdPosition, Quaternion.identity, levelsButtons[i - 1].transform);
+                        break;
+                    case 2:
+                        Instantiate(unlockedStarPrefab, firstPosition, Quaternion.identity, levelsButtons[i - 1].transform);
+                        Instantiate(unlockedStarPrefab, secondPosition, Quaternion.identity, levelsButtons[i - 1].transform);
+                        Instantiate(lockedStarPrefab, thirdPosition, Quaternion.identity, levelsButtons[i - 1].transform);
+                        break;
+                    case 3:
+                        Instantiate(unlockedStarPrefab, firstPosition, Quaternion.identity, levelsButtons[i - 1].transform);
+                        Instantiate(unlockedStarPrefab, secondPosition, Quaternion.identity, levelsButtons[i - 1].transform);
+                        Instantiate(unlockedStarPrefab, thirdPosition, Quaternion.identity, levelsButtons[i - 1].transform);
+                        break;
+                    default: // case 0
+                        Instantiate(lockedStarPrefab, firstPosition, Quaternion.identity, levelsButtons[i - 1].transform);
+                        Instantiate(lockedStarPrefab, secondPosition, Quaternion.identity, levelsButtons[i - 1].transform);
+                        Instantiate(lockedStarPrefab, thirdPosition, Quaternion.identity, levelsButtons[i - 1].transform);
+                        break;
+                }
+
             }
             else
             {
