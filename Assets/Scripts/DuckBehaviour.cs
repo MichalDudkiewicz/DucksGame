98fbae55a66e02890eb0fb71688a03b3499ce054
Michal Dudkiewicz <michal.dudkiewicz96@gmail.com>
2022-06-16 13:40:30 +0200

    revert unrelated changes

--- Assets/Scripts/DuckBehaviour.cs
+++ Assets/Scripts/DuckBehaviour.cs
@@ -45,7 +45,7 @@ public class DuckBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (life < 99)
+        if (life < 5)
         {
             GameEvents.current.DuckDeath(this.gameObject.GetComponent<DuckBehaviour>());
             DeathAnimation();
